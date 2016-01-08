b85ba4c5df58dc7346203bcba3e729913af72e63
Jeffrey Martín <jefdonmar@gmail.com>
2016-01-07 21:52:03 -0500

    Figured out that I was over thinking the function and added it to my app/index.html

--- app/js/main.js
+++ app/js/main.js
@@ -6,17 +6,8 @@ Object.defineProperty(exports, '__esModule', {
 });
 var LikeButtonController = function LikeButtonController($scope) {
 
-  $scope.words = 'like';
-  $scope.words = 'likes';
-
   $scope.doSomething = function () {
     $scope.count = $scope.count + 1;
-
-    if ($scope.count === 1) {
-      $scope.words = 'like';
-    } else {
-      $scope.words = 'likes';
-    }
   };
 };
 
--- js/controllers/likeButton_controller.js
+++ js/controllers/likeButton_controller.js
@@ -1,16 +1,8 @@
 let LikeButtonController = function ($scope) {
 
-  $scope.words = 'like';
-  $scope.words = 'likes';
 
   $scope.doSomething = function () {
     $scope.count = $scope.count + 1;
-
-    if ($scope.count === 1) {
-      $scope.words = 'like';
-    } else {
-      $scope.words = 'likes';
-    }
   };
 
 };
